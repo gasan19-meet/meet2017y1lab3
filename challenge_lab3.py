d6163afe34fe79cb898ba35274324ac65f2fee0b
gasan19-meet <gasan19@meet.mit.edu>
2017-07-25 18:13:49 +0300

    Stopping here

---
+++ challenge_lab3.py
@@ -0,0 +1,17 @@
+
+       
+print('row'*3+'your boat')
+print('gently down the stream.')
+print('merrily'*4)
+print('life is but a stream')
+
+
+print()
+print()
+
+
+print('and i was like')
+print('baby')
+print('ooooooooh')
+print('baby')
+print('nooooooooo')
---
+++ helloworld.py
@@ -0,0 +1,19 @@
+
+print('hello world')
+
+print ('hello mohee')
+
+ #print ('hello world')
+print('Γειά σου Κόσμε')
+
+print ('Olá Mundo')
+print('Ahoj světe')
+
+print('안녕 세상')
+
+print('안녕 세상')
+
+print('안녕 세상')
+print('Salamu, Dunia')
+
+ 
